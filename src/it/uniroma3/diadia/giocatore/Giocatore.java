e1f4880cde660740832df58f1bf04281ab0039b0
riccardo1512 <111280428+riccardo1512@users.noreply.github.com>
2025-04-03 18:51:50 +0200

    Add files via upload

---
+++ src/it/uniroma3/diadia/giocatore/Giocatore.java
@@ -0,0 +1,47 @@
+package it.uniroma3.diadia.giocatore;
+
+import it.uniroma3.diadia.attrezzi.Attrezzo;
+
+public class Giocatore {
+	
+	static final private int CFU_INIZIALI = 20;
+	
+	private int cfu;
+	private Borsa borsa;
+	
+	public Giocatore() {
+		
+		this.cfu = CFU_INIZIALI;
+		this.borsa = new Borsa();
+	}
+	
+	public void prendi(Attrezzo attrezzo) {
+		
+		this.borsa.addAttrezzo(attrezzo);
+	}
+
+	public void posa(Attrezzo attrezzo) {
+
+		this.borsa.removeAttrezzo(attrezzo);
+	}
+	
+	
+	
+	
+	public int getCfu() {
+		return this.cfu;
+	}
+
+	public void setCfu(int cfu) {
+		this.cfu = cfu;		
+	}
+
+	public Borsa getBorsa() {
+		return borsa;
+	}
+
+	public void setBorsa(Borsa borsa) {
+		this.borsa = borsa;
+	}	
+
+}
---
+++ testDiadia/it/uniroma3/diadia/GiocatoreTest.java
@@ -0,0 +1,117 @@
+package it.uniroma3.diadia;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.uniroma3.diadia.attrezzi.Attrezzo;
+import it.uniroma3.diadia.giocatore.Giocatore;
+
+class GiocatoreTest {
+	
+	private Giocatore giocatore;
+	private Attrezzo spada;
+    private Attrezzo scudo;
+    private Attrezzo armatura;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		
+		this.giocatore = new Giocatore();
+		this.spada = new Attrezzo("spada", 5);
+        this.scudo = new Attrezzo("scudo", 3);
+        this.armatura = new Attrezzo("armatura", 7);
+	}
+	
+	 /* TEST DEL METODO getCfu() */
+
+    @Test
+    void testCfuIniziali() {
+        assertEquals(20, this.giocatore.getCfu());
+    }
+    
+    /* TEST DEL METODO setCfu() */
+
+    @Test
+    void testSetCfu() {
+        this.giocatore.setCfu(15);
+        assertEquals(15, this.giocatore.getCfu());
+    }
+    
+    /* TEST DEL METODO getBorsa() */
+
+    @Test
+    void testBorsaNonNull() {
+        assertNotNull(this.giocatore.getBorsa());
+    }
+    
+    @Test
+	void testBorsaNull() {
+		this.giocatore.setBorsa(null);
+		assertNull(this.giocatore.getBorsa());
+	}
+
+    @Test
+    void testBorsaVuotaInizialmente() {
+        assertTrue(this.giocatore.getBorsa().isEmpty());
+    }
+	
+	
+    /* TEST DEL METODO prendi() */
+    
+    @Test
+    void testPrendiAttrezzo() {
+    	assertFalse(this.giocatore.getBorsa().hasAttrezzo("spada"));
+        this.giocatore.prendi(this.spada);
+        assertTrue(this.giocatore.getBorsa().hasAttrezzo("spada"));
+    }
+    
+    @Test
+    void testPrendiAttrezziMultipli() {
+    	assertFalse(this.giocatore.getBorsa().hasAttrezzo("spada"));
+    	assertFalse(this.giocatore.getBorsa().hasAttrezzo("scudo"));
+    	this.giocatore.prendi(this.spada);
+    	this.giocatore.prendi(this.scudo);
+    	assertTrue(this.giocatore.getBorsa().hasAttrezzo("spada"));
+    	assertTrue(this.giocatore.getBorsa().hasAttrezzo("scudo"));
+    }
+    
+    @Test
+    void testPrendiTroppiAttrezzi() {
+    	assertFalse(this.giocatore.getBorsa().hasAttrezzo("spada"));
+    	assertFalse(this.giocatore.getBorsa().hasAttrezzo("armatura"));
+    	this.giocatore.prendi(this.spada);
+    	this.giocatore.prendi(this.armatura);
+    	assertTrue(this.giocatore.getBorsa().hasAttrezzo("spada"));
+    	assertFalse(this.giocatore.getBorsa().hasAttrezzo("armatura"));	// giocatore non ha preso l'armatura perché troppo pesante
+    }
+    
+    /* TEST DEL METODO posa() */
+
+    @Test
+    void testPosaAttrezzo() {
+        this.giocatore.prendi(this.spada);
+        this.giocatore.posa(this.spada);
+        assertFalse(this.giocatore.getBorsa().hasAttrezzo("spada"));
+    }
+    
+    @Test
+    void testPosaAttrezziMultipli() {
+    	this.giocatore.prendi(this.spada);
+        this.giocatore.prendi(this.scudo);
+        this.giocatore.posa(this.spada);
+        this.giocatore.posa(this.scudo);
+        assertFalse(this.giocatore.getBorsa().hasAttrezzo("spada"));
+        assertFalse(this.giocatore.getBorsa().hasAttrezzo("scudo"));
+    	
+    }
+
+    @Test
+    void testPosaAttrezzoNonPresente() {
+        assertFalse(this.giocatore.getBorsa().hasAttrezzo("spada"));
+        this.giocatore.posa(spada);
+        assertFalse(this.giocatore.getBorsa().hasAttrezzo("spada"));
+    }
+
+}
